ec9fc262d39e62314a1b2f3ba0045b6bf6eaf493
nicolascagna96 <nicolascagna@hotmail.com>
2022-06-01 14:23:26 +0000

    Collect last 5 entries from sales worksheet

--- run.py
+++ run.py
@@ -36,7 +36,6 @@ def get_sales_data():
 
     return sales_data
 
-
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -55,6 +54,24 @@ def validate_data(values):
 
     return True
 
+# def update_sales_worksheet(data):
+  #  """
+  #  Update sales worksheet, add new row with the list data provided
+   # """
+   # print('Updating sales worksheet...\n')
+   # sales_worksheet = SHEET.worksheet("sales")
+   #  sales_worksheet.append_row(data)
+   # print("Sales worksheet updated successfully.\n")
+
+# def update_surplus_worksheet(data):
+  #  """
+   # Update the surplus worksheet adding a new row to it
+   # """
+   # print('Updating surplus worksheet...\n')
+   # surplus_worksheet = SHEET.worksheet("surplus")
+   # surplus_worksheet.append_row(data)
+   # print("Surplus worksheet updated successfully.\n")
+
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
@@ -64,9 +81,6 @@ def update_worksheet(data, worksheet):
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
-
-
-
 
 def calculate_surplus_data(sales_row):
     """
@@ -86,6 +100,22 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+        
+
 def main():
     """ 
     Run all program functions
@@ -98,4 +128,5 @@ def main():
 
 
 print("Welcome to Love Sandwiches Automation")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
